ca2e2d084dd0fb7239c21933d5df53bbcdd6e891
tao liu <taoliu@taodeMacBook-Pro.local>
2019-04-10 09:19:38 +0800

    增加 创建本地splash脚本

---
+++ create_splash.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker run -itd -p 8051:8050 -m 500m --memory-swap 500m scrapinghub/splash
+
+docker run -itd -p 8052:8050 -m 500m --memory-swap 500m scrapinghub/splash
+
+docker run -itd -p 8053:8050 -m 500m --memory-swap 500m scrapinghub/splash
+
+docker run -itd -p 8054:8050 -m 500m --memory-swap 500m scrapinghub/splash
+
+
+docker run -itd -p 8055:8050 -m 500m --memory-swap 500m scrapinghub/splash
