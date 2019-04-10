032d9f35ed1a1ba42fa61c8539d6a458058406ea
tao liu <taoliu@taodeMacBook-Pro.local>
2019-04-10 09:44:16 +0800

    FIX ERROR

--- create_splash.sh
+++ create_splash.sh
@@ -6,7 +6,7 @@ docker run -itd -p 8052:8050 -m 500m --memory-swap 500m --restart always scrapin
 
 docker run -itd -p 8053:8050 -m 500m --memory-swap 500m --restart always scrapinghub/splash
 
-docker run -itd -p 8054:8050 -m 500m --memory-swap 500m--restart always scrapinghub/splash
+docker run -itd -p 8054:8050 -m 500m --memory-swap 500m --restart always scrapinghub/splash
 
 
 docker run -itd -p 8055:8050 -m 500m --memory-swap 500m --restart always scrapinghub/splash
